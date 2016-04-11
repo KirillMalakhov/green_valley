23760a4de9355c23e672ed94c3546aaca48a0872
Kirill74 <vtnhjy2832@gmail.com>
2016-04-11 23:59:17 +0300

    check

--- src/blocks/promo-slider/tabs.js
+++ src/blocks/promo-slider/tabs.js
@@ -1,12 +1,10 @@
-$( document ).ready(function() {
-        var linkHref;
-        $('.promo-slider__tabs-item a').on('click', function(event){
-          event.preventDefault();
-          $(this).closest('.promo-slider-tabs').find('.promo-slider__tabs-item.promo-slider__tabs-item--active').removeClass('promo-slider__tabs-item--active')
-          $(this).closest('.promo-slider__tabs-item').addClass('promo-slider__tabs-item--active');
-
-          $(this).closest('.promo-slider-tabs').find('.promo-slider__tabs-item.promo-slider__tabs-item--active').removeClass('promo-slider__tabs-item--active');
-          linkHref = $(this).attr('href');
-          $(linkHref).addClass('promo-slider__tabs-item--active');
-        });
-  });
+var linkHref;
+$('.tabs-link a').on('click', function(event){
+  event.preventDefault();
+  $(this).closest('.tabs').find('.tabs-link.active').removeClass('active')
+  $(this).closest('.tabs-link').addClass('active');
+  
+  $(this).closest('.tabs').find('.tab-item.active').removeClass('active');
+  linkHref = $(this).attr('href');
+  $(linkHref).addClass('active');
+});
