aa594fe56f0c74dbe6ab1f4253f31390b7a7762a
Kirill74 <vtnhjy2832@gmail.com>
2016-04-21 21:57:01 +0300

    third page

--- src/blocks/owl-carousel/owl.carousel.js
+++ src/blocks/owl-carousel/owl.carousel.js
@@ -3103,3 +3103,19 @@ $('.owl-carousel-2').owlCarousel({
         }
     }
 })
+$('.owl-carousel-3').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
